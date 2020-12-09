a8f14e5733951cb3ae7e1fcf7fd00ca89629fd19
Luchin101 <luis.santanta101@gmail.com>
2020-12-09 17:54:17 -0300

    change testing params for production params

--- corfogeneratecode/settings/common.py
+++ corfogeneratecode/settings/common.py
@@ -1,6 +1,6 @@
 def plugin_settings(settings):
-    settings.CORFOGENERATE_URL_TOKEN = 'https://apitest.corfo.cl:9101/api/oauth/token'
-    settings.CORFOGENERATE_CLIENT_ID = '3caba124-fb3b-4bee-909f-af7edfe9b448'
-    settings.CORFOGENERATE_CLIENT_SECRET = 'af003869-fbac-4d0f-982d-f3515faa70bd'
-    settings.CORFOGENERATE_URL_VALIDATE = 'https://apitest.corfo.cl:9101/OAG/API_WS_MOOC/Validate'
-    settings.CORFOGENERATE_ID_INSTITUTION = '50'
+    settings.CORFOGENERATE_URL_TOKEN = 'https://www.corfo.cl/oauth/token'
+    settings.CORFOGENERATE_CLIENT_ID = ''
+    settings.CORFOGENERATE_CLIENT_SECRET = ''
+    settings.CORFOGENERATE_URL_VALIDATE = 'https://www.corfo.cl/api/StartupJourneyWSApiMooc/Validate'
+    settings.CORFOGENERATE_ID_INSTITUTION = ''
