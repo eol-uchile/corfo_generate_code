6f9dadf74767723395d4e1186283693059093479
matsalinas <matsalinas@uchile.cl>
2021-03-11 18:38:11 +0000

    Update tests koa

--- .github/test.sh
+++ .github/test.sh
@@ -2,12 +2,12 @@
 pip install -e git+https://github.com/eol-uchile/uchileedxlogin@9c9b4ee047515026ce71cbd8aa1a457d2c3d4e5e#egg=uchileedxlogin
 pip install -e /openedx/requirements/corfo_generate_code
 
-cd /openedx/requirements/corfo_generate_code/corfogeneratecode
+cd /openedx/requirements/corfo_generate_code
 cp /openedx/edx-platform/setup.cfg .
 mkdir test_root
 cd test_root/
 ln -s /openedx/staticfiles .
 
-cd /openedx/requirements/corfo_generate_code/corfogeneratecode
+cd /openedx/requirements/corfo_generate_code
 
-DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest tests.py
+DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest corfogeneratecode/tests.py
