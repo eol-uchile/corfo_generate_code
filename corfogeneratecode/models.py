b1f6320fd45aaf069df13cb31037427995f56193
Luchin101 <luis.santanta101@gmail.com>
2021-04-06 17:03:18 -0400

    minior change model

--- corfogeneratecode/models.py
+++ corfogeneratecode/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
 from django.db import models
-from datetime import datetime
 from opaque_keys.edx.django.models import CourseKeyField
 
 # Create your models here.
@@ -24,7 +23,7 @@ class CorfoCodeUser(models.Model):
         User,
         on_delete=models.CASCADE)
     code = models.CharField(max_length=20)
-    created_at = models.DateTimeField(default=datetime.now, blank=True)
+    created_at = models.DateTimeField(auto_now=True, blank=True)
     mapping_content = models.ForeignKey(
         CorfoCodeMappingContent,
         on_delete=models.CASCADE,
