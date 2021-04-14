815632d85e8980df5f13eb0ca17f1ca228e2d68f
Luchin101 <luis.santanta101@gmail.com>
2021-04-14 12:40:39 -0400

    Set user field as raw id

--- corfogeneratecode/admin.py
+++ corfogeneratecode/admin.py
@@ -5,6 +5,7 @@ from .models import CorfoCodeUser, CorfoCodeMappingContent, CorfoCodeInstitution
 
 
 class CorfoCodeUserAdmin(admin.ModelAdmin):
+    raw_id_fields = ('user',)
     list_display = ('user', 'mapping_content', 'code', 'corfo_save', 'created_at')
     search_fields = ['user__username', 'mapping_content__id_content','mapping_content__content', 'code', 'created_at']
     ordering = ['-created_at']
